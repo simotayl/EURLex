f7c1fb232ae2e5ad2af43396fcf6297fea05909f
Simon Taylor <simotayl@gmail.com>
2019-12-30 09:32:02 +0000

    initial working model, with improvements needed for older PDFs

---
+++ app.R
@@ -0,0 +1,221 @@
+library(shiny)
+library(DT)
+
+library(readr)
+library(pdftools)
+
+library(dplyr)
+library(lubridate)
+library(janitor)
+library(stringr)
+
+source("legislation_mining.R")
+
+leg_raw <- read.csv("Data/EULex-agri.csv",stringsAsFactors = FALSE)
+
+leg_raw$Title <- iconv(leg_raw$Title,"latin1", "UTF-8", sub="") #Deletes unreadable unicode
+
+#https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:22019A0605(01)
+
+leg_data <- leg_raw %>%
+  mutate(`Subject matter` = strsplit(as.character(`Subject.matter`),", "),
+         `EUROVOC descriptor` = strsplit(as.character(`EUROVOC.descriptor`),", "),
+         leg_year = year(as.Date(`Date.of.document`,format = "%d/%m/%Y")))
+
+subjects <- unique(Reduce(c,leg_data$`Subject matter`))
+descriptors <- unique(Reduce(c,leg_data$`EUROVOC descriptor`))
+
+years <- unique(leg_data$`leg_year`) %>% sort(decreasing = TRUE)
+
+pdfURL <- ""
+legislation_text <- list()
+
+
+ui <- tagList(
+  
+  # Application title
+  navbarPage("EU legislation finder",id = "main",
+    tabPanel("Legislation search",
+      sidebarPanel(
+        selectInput("filtertype",
+                    "Search by:",
+                    choices = c("EUROVOC descriptor" = "descriptor","Subject matter" = "subject"),
+                    selected = "EUROVOC descriptor"),
+        conditionalPanel("input.filtertype == 'descriptor'",
+                         selectizeInput("descriptorselect",
+                                        "EUROVOC descriptor:",
+                                        choices = descriptors,
+                                        multiple = TRUE)
+        ),
+        conditionalPanel("input.filtertype == 'subject'",
+                         selectizeInput("subjectselect",
+                                        "Subject matter:",
+                                        choices = subjects,
+                                        multiple = TRUE)
+        ),
+        checkboxInput("allanyflag",
+                      "Match all criteria?",
+                      value = FALSE),
+        textInput("titlestring",
+                  "Search for specific legislation",
+                  placeholder = "You can enter a specific search string here"),
+        selectInput("yearselect",
+                    "Year of publication:",
+                    choices = c("All",years)
+        ),
+        "Use the tools above to search for active legislation. You can then select the legislation in the table and use the button
+        below to search for linked legislation. Alternatively you can navigate to the linked legislation tab above and enter the
+        legislation directly",
+        actionButton("searchselected",
+                     "Find linked legislation")
+      ),
+      
+      mainPanel(
+        dataTableOutput("searchtable")
+      )
+    ),
+    tabPanel("Linked legislation",
+      sidebarPanel(
+        selectInput("pdfselect",
+                       "Choose legislation to search",
+                       choices = c("",leg_data$Title)),
+        textInput("pdftext",
+                  "Alternatively, enter the URL here",
+                  placeholder = "Paste the PDF URL or use the search above"
+                  ),
+        checkboxGroupInput("legtype",
+                           "Legislation types to show",
+                           choices = c("Directive","Decision","Regulation"),
+                           selected = c("Directive","Decision","Regulation")
+                           ),
+        checkboxInput("activeonlyflag",
+                      "Show only active legislation?",
+                      value = FALSE)
+      ),
+      mainPanel(
+        dataTableOutput("linktable")
+      )
+    )
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+  leg_output <- reactive({
+    if(input$filtertype == "descriptor"){
+      if(input$allanyflag == TRUE){
+        leg_output <- leg_data %>% filter(sapply(`EUROVOC descriptor`,function(x){length(intersect(input$descriptorselect,unlist(x)))==length(input$descriptorselect)&&length(input$descriptorselect)>0}))
+      }
+      else{
+        leg_output <- leg_data %>% filter(sapply(`EUROVOC descriptor`,function(x){length(intersect(input$descriptorselect,unlist(x)))>0}))
+      }
+      if(length(input$descriptorselect) == 0 & input$titlestring !=""){leg_output <- leg_data}
+    }
+    else{
+      if(input$allanyflag == TRUE){
+        leg_output <- leg_data %>% filter(sapply(`Subject matter`,function(x){length(intersect(input$subjectselect,unlist(x)))==length(input$subjectselect)&&length(input$subjectselect)>0}))
+      }
+      else{
+        leg_output <- leg_data %>% filter(sapply(`Subject matter`,function(x){length(intersect(input$subjectselect,unlist(x)))>0}))
+      }
+      if(length(input$subjectselect) == 0 & input$titlestring !=""){leg_output <- leg_data}
+    }
+    
+    if(input$titlestring !=""){leg_output <- leg_output %>% filter(grepl(input$titlestring,Title,ignore.case = T))}
+    
+    if(input$yearselect != "All"){leg_output <- leg_output %>% filter(leg_year == input$yearselect)}
+    
+
+    
+    return(leg_output)
+  })
+  
+  observeEvent(input$searchselected,{
+    req(length(input$searchtable_rows_selected)>0)
+    CELEX_selected <- leg_output()$CELEX[input$searchtable_rows_selected]
+    pdfURL <- paste0("https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:",CELEX_selected)
+    updateTextInput(session,"pdftext",value = pdfURL)
+    updateTabsetPanel(session, "main",
+                      selected = "Linked legislation")
+    
+  })
+  
+  observeEvent(input$pdftext,{
+    req(input$pdftext!="")
+    pdfURL <- input$pdftext
+    legislation_text <- list()
+    validate(need(
+      try(legislation_text <<- pdf_text(pdfURL),silent = TRUE),message = "URL not found"))
+    
+    updateSelectInput(session,"pdfselect", selected = "")    
+  })
+  
+  observeEvent(input$pdfselect,{
+    req(input$pdfselect!="")
+    CELEX_selected <- leg_data$CELEX[which(leg_data$Title == input$pdfselect)]
+    pdfURL <- paste0("https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:",CELEX_selected)
+    legislation_text <<- pdf_text(pdfURL)   
+    
+    updateTextInput(session,"pdftext", value = "")
+    })  
+  
+    
+  references_generate <- reactive({
+    req(input$pdftext!=""|input$pdfselect!="")
+    req(length(legislation_text)>0)
+#    legislation_text <- sapply(legislation_text, function(text) str_replace_all(text," ","")) #to test
+    return(
+      bind_rows(
+        find_references(legislation_text,leg_data,"Directive","L","(?<=Directive ).*(?=/E)"),
+        find_references(legislation_text,leg_data,"Decision","D","(?<=Decision ).*(?=/E)"),
+        find_references(legislation_text,leg_data,"Regulation","R","(?<=Regulation \\(E.\\) ).*(/....)")
+      ) %>% filter(Title!="")
+    )
+  })
+  
+  references_output <- reactive({
+    references_table <- references_generate() %>%
+      filter(Category %in% input$legtype)
+
+    if(input$activeonlyflag == TRUE){
+      references_table <- references_table %>%
+        filter(MatchFlag == 1)
+    }
+    return(references_table)
+  })
+  
+  
+  output$searchtable <- renderDT({
+    datatable({leg_output() %>%
+                mutate(Link = paste0("<a href='https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:",`CELEX.number`,"' target='_blank'>Link</a>"),
+                       PDF = paste0("<a href='https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:",`CELEX.number`,"' target='_blank'>PDF</a>")) %>% 
+                select(Title,`Date of document` = `Date.of.document`,Link,PDF)},
+              escape = FALSE,
+              rownames = FALSE,
+              extensions = 'Scroller',
+              selection = list(mode = "single", target = 'row'),
+              options = list(scrollY = 800,
+                             paging = FALSE,
+                             scrollCollapse = TRUE))
+  })
+  
+  output$linktable <- renderDT({
+    datatable(references_output() %>%
+                select(Title,Category,Pages,`CELEX number`,Link,PDF),
+              escape = FALSE,
+              rownames = FALSE,
+              extensions = 'Scroller',
+              selection = "single",
+              options = list(scrollY = 800,
+                             paging = FALSE,
+                             scrollCollapse = TRUE))
+  })
+  
+  
+  
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ legislation_mining.R
@@ -0,0 +1,107 @@
+library(pdftools)
+library(stringr)
+library(dplyr)
+library(readr)
+library(DT)
+
+# EU_lex <- read.csv("Data/EULex-agri.csv",stringsAsFactors = FALSE)
+# EU_lex$Title <- iconv(EU_lex$Title,"latin1", "ASCII", sub="") #Deletes unreadable unicode
+# 
+# CELEX_test <- "31991L0412"
+# 
+# legislation_text <- pdf_text(paste0("https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:",CELEX_test))
+# 
+# leg_title <- pdf_info("https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:32019R0006")$keys$Title
+
+#https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:32019R0006
+#https://eur-lex.europa.eu/content/tools/TableOfSectors/types_of_documents_in_eurlex.html
+
+
+output <- bind_rows(
+  find_references(legislation_text,EU_lex,"Directive","L","(?<=Directive ).*(?=/E)"),
+  find_references(legislation_text,EU_lex,"Decision","D","(?<=Decision ).*(?=/E)"),
+  find_references(legislation_text,EU_lex,"Regulation","R","(?<=Regulation \\(E.\\) ).*(/....)")
+) %>% filter(Title!="")
+
+find_references <- function(legislation_text,EU_lex,type,index,string_pattern){
+  match_base <- sapply(legislation_text,function(text) {
+    str_match <- str_extract_all(string = text, pattern = string_pattern)
+    str_match <- unlist(sapply(str_match,function(string) str_replace_all(string,"\\.","")))
+  }) #finds all instances of the code.
+  
+  match_simple <- sapply(match_base,function(text){
+    str_match <- text[!startsWith(text,"No")]
+    str_match <- sapply(str_match, function(string) word(string,1))
+  }) #finds all instances not preceeded by 'No'
+  
+  match_no <- sapply(match_base,function(text){
+    str_match <- text[startsWith(text,"No")]
+    str_match <- sapply(str_match, function(string) word(unlist(gsub("No ","",string)),1))
+  }) #finds all other instances
+  
+  bind_rows(
+    reference_table(EU_lex,match_simple,type,index),
+    reference_table(EU_lex,match_no,type,index,reverse = TRUE)
+  )
+}
+
+reference_table <- function(EU_lex,matched_codes,type,index,reverse = FALSE){
+    
+  code <- na.omit(unique(unlist(matched_codes))) #list of all the codes found
+  pages <- sapply(code,function(x) paste(grep(x,matched_codes),collapse = ", ")) #this creates a comma separated list of pages with matches
+  code <- str_replace_all(code," ","")
+  if(reverse == TRUE){
+    #optional version to account for two different orentations of year and reference in the codes
+    CELEX <-sapply(str_split(code,"/"), #function to generate CELEX IDs from the legislation code
+                   function(x) {paste0(3, #all legislation has this code at the start
+                                       ifelse(str_length(x[2])==2,paste0("19",x[2]),x[2]), #to correct for 1995 appearing as 95 etc
+                                       index, #the letter code - see reference file
+                                       strrep("0",4-str_length(x[1])), #adds in leading zeros if needed
+                                       x[1]) }) 
+    
+  }
+  else {
+    #identical to the above, but with the str split references reversed
+    CELEX <-sapply(str_split(code,"/"), #function to generate CELEX IDs from the legislation code
+                          function(x) {paste0(3, #all legislation has this code at the start
+                                             ifelse(str_length(x[1])==2,paste0("19",x[1]),x[1]), #to correct for 1995 appearing as 95 etc
+                                             index, #the letter code - see reference file
+                                             strrep("0",4-str_length(x[2])), #adds in leading zeros if needed
+                                             x[2]) })
+  }
+  if(length(code)>0){
+    directive_table <- tibble(Category = type,Pages = pages,`CELEX number` = CELEX, code = code) %>%
+      left_join(EU_lex,by = c("CELEX number" = "CELEX.number")) %>%
+      mutate(
+        MatchFlag = ifelse(is.na(Publication.Reference),0,1),
+        Title = ifelse(is.na(Title),paste("<i>",Category,code,"(inactive)</i>"),Title),
+        Link = paste0("<a href='https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:",`CELEX number`,"' target='_blank'>Link</a>"),
+        PDF = paste0("<a href='https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:",`CELEX number`,"' target='_blank'>PDF</a>"))
+  }
+  else{directive_table <- tibble(NA)} #this ensures bind_rows will not have any problems
+}
+
+
+
+
+
+# string_pattern <- "(?<=Directive ).*(?=/E)" #string search pattern. For Directives it searches for "Directive [Code]/E" and returns [Code]
+# matched_codes <- sapply(legislation_text,function(text) {
+#   str_extract_all(string = text, pattern = string_pattern)}) #finds all instances of the code. TO IMPROVE
+# directives <- reference_table(matched_codes,"Directive","L")
+
+
+#need to split in same manner as regulations
+#string_pattern <- "(?<=Decision ).*(?=/E)" #string search pattern. For Directives it searches for "Directive [Code]/E" and returns [Code]
+
+#decisions <- reference_table(matched_codes,"Decision","D")
+
+
+#string_pattern <- "(?<=Regulation \\(E.\\) ).*(/....)" #string search pattern. For Directives it searches for "Directive [Code]/E" and returns [Code]
+
+
+
+#bind_rows(,decisions,regulations_simple,regulations_no)
+
+#datatable(output,escape = F)
+
