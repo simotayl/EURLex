b5ad5e06217524231accc50e5cd06926e6dc7652
p998872 <simon.taylor1@defra.gov.uk>
2020-01-15 12:39:24 +0000

    Comments added, extra status line added from HTML data.

--- legislation_mining.R
+++ legislation_mining.R
@@ -4,56 +4,59 @@ library(dplyr)
 library(readr)
 library(DT)
 
-# EU_lex <- read.csv("Data/EULex-agri.csv",stringsAsFactors = FALSE)
-# EU_lex$Title <- iconv(EU_lex$Title,"latin1", "ASCII", sub="") #Deletes unreadable unicode
-# 
-# CELEX_test <- "32019R0158"
-# 
-# legislation_text <- pdf_text(paste0("https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:",CELEX_test))
-# 
-# leg_title <- pdf_info("https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:32019R0006")$keys$Title
-
-#https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:32019R0006
-#https://eur-lex.europa.eu/content/tools/TableOfSectors/types_of_documents_in_eurlex.html
-
-# string_pattern <- "(?<=Regulation\\(E.\\))/...."
-
-# output <- bind_rows(
-#   find_references(legislation_text,EU_lex,"Directive","L","(?<=Directive ).*(?=/E)"),
-#   find_references(legislation_text,EU_lex,"Decision","D","(?<=Decision ).*(?=/E)"),
-#test <- find_references(legislation_text,EU_lex,"Regulation","R","(?<=Regulation\\(E.\\)).*(/....)")
-# ) %>% filter(Title!="")
-
-
 find_references <- function(legislation_text,EU_lex,type,index,string_pattern){
+  #This function takes text read from a pdf (a list of strings, one for each page of the pdf) along with a
+  #string matching REGEX patter and returns the corresponding table of identified references. The function
+  #does the string matching, and then passes the results to a subfunction reference_table to generate the output.
+  
+  #We first identify all matching strings using str_extract_all, which returns a vector of all the matches.
+  #The sapply wrapper iterates through the list, returning a list of vectors. 
   match_base <- sapply(legislation_text,function(text) {
+    #first remove all spaces from the code (this is done because old scanned pdfs have unreliable spacing)
     text_condensed <- str_replace_all(text," ","")
+    #Then run the match against the specified REGEX.
     str_match <- str_extract_all(text_condensed, pattern = string_pattern)
+    #We then eliminate everythig that isn't a digit, a dash or the letter N (latter explained below)
     str_match <- unlist(sapply(str_match,function(string) str_replace_all(string,"[^0-9N/]","")))
-  }) #finds all instances of the code.
+  })
   
+  #Legislation references are typically either xxxx/year or No. year/xxxx. Because the year is the other way
+  #around, we split these cases based on the leading letter N.
+  
+  #finds all instances not preceeded by 'No' (or just 'N' given the replace above)
   match_simple <- sapply(match_base,function(text){
+    #Pick up all matches without an N at the start
     str_match <- text[!startsWith(text,"N")]
+    #dump any redundant text left at the end
     str_match <- sapply(str_match, function(string) word(string,1))
-  }) #finds all instances not preceeded by 'No'
+  })
   
+  #finds all other instances
   match_no <- sapply(match_base,function(text){
+    #Pick up those with the N at the start
     str_match <- text[startsWith(text,"N")]
+    #Remove the N and any other redundant text.
     str_match <- sapply(str_match, function(string) word(unlist(str_replace_all(string,"N","")),1))
-  }) #finds all other instances
+  })
   
-  bind_rows(
-    reference_table(EU_lex,match_simple,type,index),
-    reference_table(EU_lex,match_no,type,index,reverse = TRUE)
-  )
+  #send both versions of data to the table function below and return combined result
+  return(bind_rows(
+    #Note the optional reverse variable - this is to account for the year/ref being swapped
+      reference_table(EU_lex,match_simple,type,index),
+      reference_table(EU_lex,match_no,type,index,reverse = TRUE)
+  ))
 }
 
 reference_table <- function(EU_lex,matched_codes,type,index,reverse = FALSE){
-  code <- na.omit(unique(unlist(matched_codes))) #list of all the codes found
+  #this generates an output table from matched code strings.
+  
+  #the input is a list of vectors of all the string matches (each list element is a page of the source)
+  
+  code <- na.omit(unique(unlist(matched_codes))) #generate a single list of all the codes found
   code <- code[str_length(code)<10] #remove erroneous long codes
   
   pages <- sapply(code,function(x) paste(grep(x,matched_codes),collapse = ", ")) #this creates a comma separated list of pages with matches
-  code <- str_replace_all(code," ","")
+  
   if(reverse == TRUE){
     #optional version to account for two different orentations of year and reference in the codes
     CELEX <-sapply(str_split(code,"/"), #function to generate CELEX IDs from the legislation code
@@ -73,40 +76,19 @@ reference_table <- function(EU_lex,matched_codes,type,index,reverse = FALSE){
                                              strrep("0",4-str_length(x[2])), #adds in leading zeros if needed
                                              x[2]) })
   }
+  #If codes were found, generate the table and links
   if(length(code)>0){
+    #create a tibble
     directive_table <- tibble(Category = type, Pages = pages, `CELEX number` = CELEX, code = code) %>%
+      #join the active legislation csv data
       left_join(EU_lex,by = "CELEX number") %>%
       mutate(
-        MatchFlag = ifelse(is.na(`Publication Reference`),0,1),
-        Title = ifelse(is.na(Title),paste("<i>",Category,code,"(amended/not in force)</i>"),Title),
-        Link = paste0("<a href='https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:",`CELEX number`,"' target='_blank'>Link</a>"),
+        MatchFlag = ifelse(is.na(`Publication Reference`),0,1), #was there a match to active legislation?
+        Title = ifelse(is.na(Title),paste("<i>",Category,code,"(amended/not in force)</i>"),Title), #Use the title from the data, or a default if not found
+        Link = paste0("<a href='https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:",`CELEX number`,"' target='_blank'>Link</a>"), #hyperlinks
         PDF = paste0("<a href='https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:",`CELEX number`,"' target='_blank'>PDF</a>")) %>%
-      select(Title,Category,Pages,`CELEX number`,Link,PDF,MatchFlag)
+      select(Title,Category,Pages,`CELEX number`,Link,PDF,MatchFlag) #select what we need
   }
-  else{directive_table <- tibble(NA)} #this ensures bind_rows will not have any problems
+  else{directive_table <- tibble(NA)} #this ensures subsequent bind_rows will not have any problems
 }
-
-
-
-
-
-# string_pattern <- "(?<=Directive ).*(?=/E)" #string search pattern. For Directives it searches for "Directive [Code]/E" and returns [Code]
-# matched_codes <- sapply(legislation_text,function(text) {
-#   str_extract_all(string = text, pattern = string_pattern)}) #finds all instances of the code. TO IMPROVE
-# directives <- reference_table(matched_codes,"Directive","L")
-
-
-#need to split in same manner as regulations
-#string_pattern <- "(?<=Decision ).*(?=/E)" #string search pattern. For Directives it searches for "Directive [Code]/E" and returns [Code]
-
-#decisions <- reference_table(matched_codes,"Decision","D")
-
-
-#string_pattern <- "(?<=Regulation \\(E.\\) ).*(/....)" #string search pattern. For Directives it searches for "Directive [Code]/E" and returns [Code]
-
-
-
-#bind_rows(,decisions,regulations_simple,regulations_no)
-
-#datatable(output,escape = F)
 
